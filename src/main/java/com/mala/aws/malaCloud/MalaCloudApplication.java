ee454af1bd4f1887cf712d63f20ca67c1d331e8a
adriano malaquias <adriano.malaquias@gmail.com>
2023-07-19 11:36:57 -0300

    mala commit 1

---
+++ src/main/java/com/mala/aws/malaCloud/MalaCloudApplication.java
@@ -0,0 +1,55 @@
+package com.mala.aws.malaCloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class MalaCloudApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Teste Spring");
+
+		SpringApplication.run(MalaCloudApplication.class, args);
+	}
+
+	@GetMapping("/mala/carro/{codigoCarro}")
+	public String malaGet(@PathVariable String codigoCarro){
+
+		String carro="";
+		if(codigoCarro.equals("1")){
+			carro = "Maverick";
+		}
+		if(codigoCarro.equals("2")){
+			carro = "Fusca";
+		}
+		if(codigoCarro.equals("3")){
+			carro = "Mala Carro";
+		}
+
+		return  carro;
+	}
+
+
+	@GetMapping("/giselia/placa")
+
+	public String placaGet(){ return  "{codigo: 1}, {placa:XXXX -1234}, {codigo:2}, {placa:YYYY - 4567}";
+	}
+
+	@PostMapping("/chico/gravar")
+		public String gravar(){
+		System.out.println("Gravou os dadsos");
+		return  "gravou os dados PR";
+	}
+
+
+	@GetMapping("/mala")
+
+	public String teste(){ return  "{codigo: 1}, {placa:XXXX -1234}, {codigo:2}, {placa:YYYY - 4567}";
+	}
+
+}
---
+++ src/test/java/com/mala/aws/malaCloud/MalaCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mala.aws.malaCloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MalaCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
